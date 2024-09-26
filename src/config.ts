3b19111ee993467d35113a873d3314d086e23b24
Sergiu <szatmari.sergiu@gmail.com>
2024-09-26 07:53:18 +0300

    Min -- HTTP server added for health-check purposes

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,13 @@ import { join as pathJoin } from 'path';
 export default {
   hostPrivateKeyFilePath: pathJoin(__dirname, '../id_rsa'),
 
-  port: 8022,
-  hostname: '172.29.237.87', // '127.0.0.1',
+  port: {
+    sftp: 8022,
+    web: 8080,
+  },
+  hostname: '172.29.237.87',
+  // hostname: '0.0.0.0',
+  // hostname: '127.0.0.1',
 
   allowedUser: Buffer.from('sergiu'),
   allowedPassword: Buffer.from('password'),
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { Server, ServerConfig } from 'ssh2';
 import { readFileSync } from 'fs';
+import { createServer as createWebServer } from 'http';
 import config from './config';
 import connection from './controller';
 
@@ -13,7 +14,7 @@ const serverConfig: ServerConfig = {
 }
 const server = new Server(serverConfig, connection);
 
-server.listen(config.port, config.hostname, () => {
+server.listen(config.port.sftp, config.hostname, () => {
   const address = server.address();
   if (!address) {
     console.log(`SFTP Server is not listening...`);
@@ -23,3 +24,15 @@ server.listen(config.port, config.hostname, () => {
     console.log(`SFTP Server listening on port ${address.port}`)
   }
 })
+
+const webServer = createWebServer((req, res) => {
+  res.statusCode = 200;
+  res.write('OK');
+  return res.end(
+    () => console.log(`[${new Date().toISOString()}][HTTP] Responded "200 OK" to HTTP Request @ ${req.url}`)
+  );
+});
+
+webServer.listen(config.port.web, () => {
+  console.log(`Web server (Health Check) listening on port ${config.port.web}`)
+});
